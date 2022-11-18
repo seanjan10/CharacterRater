2592c4fd8cea7330e01c0bcb3a11acc55082ef63
Sean_J <SeanJan10@hotmail.com>
2022-11-18 18:49:48 -0500

    CharacterDispaly supports multi character actors

--- client/src/components/CharacterDisplay.js
+++ client/src/components/CharacterDisplay.js
@@ -15,17 +15,36 @@ const CharacterDisplay = ({mediaID}) => {
         getCharacters();
     }, [])
     
+    function sortByEpisodeCount(a,b) {
+        if (a.episode_count > b.episode_count) {
+            return -1
+        } else {
+            return 1
+        }
+    }
+
     return (
         <div className="">
             {characters.cast &&
             characters.cast.map( (item, i) => { 
-               return (<p key={i}><b>real name </b> = {item.name} <b>TMBD id </b> = {item.id}      <b>episodes actor appeared in </b>= {item.total_episode_count} 
-               {item.roles &&
-               item.roles.map((character) => {  
-                return (<>
-                    <b> character id </b> = {character.credit_id} <b>character name </b> = {character.character}
+               return (<p key={i}><b>real name </b> = {item.name}  <b> character name </b> = 
+               {/* sort characters based on how many episodes that character appeared in if the actor played more than one character */}
+               {/* remove credit_id for formatting */}
+               {item.roles.length > 1 &&
+               item.roles.sort(sortByEpisodeCount) &&
+               item.roles.map((character, i) => {  
+                return (i + 1 === item.roles.length ) ? (<>
+                 {' '} {character.character} {' '} ({character.episode_count} Eps)
+                    </>): (<>
+                 {' '} {character.character} ({character.episode_count} Eps) / 
                     </>)
                })}
+
+               {item.roles.length === 1 &&
+               item.roles.map((character) => {  
+                return  <> {character.character} ({character.episode_count} Eps)</>
+               })}
+
                </p>)
             })}
         </div>
@@ -33,3 +52,4 @@ const CharacterDisplay = ({mediaID}) => {
 }
 
 export default CharacterDisplay
+/*<> <b> character id </b> = {character.credit_id} <b>character name </b> = {character.character} </>) */

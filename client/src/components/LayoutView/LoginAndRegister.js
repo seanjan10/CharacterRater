d10c19c5fb6518abcfbf49cb32d344d3d8d5e3b4
Sean_J <SeanJan10@hotmail.com>
2023-02-16 21:27:44 -0500

    signup context hook and component

--- client/src/components/LayoutView/LoginAndRegister.js
+++ client/src/components/LayoutView/LoginAndRegister.js
@@ -1,146 +1,155 @@
-import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import Modal from 'react-bootstrap/Modal';
-import { Link } from 'react-router-dom'
-
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Modal from "react-bootstrap/Modal";
+import Alert from "react-bootstrap/Alert"
+import { Link } from "react-router-dom";
+import { useSignup } from "../../hooks/useSignup";
 
 function LoginAndRegister() {
+  const [show1, setShow1] = useState(false);
 
-    const [show1, setShow1] = useState(false);
+  const handleClose1 = () => setShow1(false);
+  const handleShow1 = () => setShow1(true);
 
-    const handleClose1 = () => setShow1(false);
-    const handleShow1 = () => setShow1(true);
-    
-    const [show2, setShow2] = useState(false);
+  const [show2, setShow2] = useState(false);
 
-    const handleClose2 = () => setShow2(false);
-    const handleShow2 = () => setShow2(true);
+  const handleClose2 = () => setShow2(false);
+  const handleShow2 = () => setShow2(true);
 
-    const [email, setEmail] = useState('')
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
-    const [passwordRetype, setPasswordRetype] = useState('')
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordRetype, setPasswordRetype] = useState("");
+  const { signup, error, isLoading } = useSignup();
 
-    const showSignUp = () => {
-        setShow1(false);
-        setShow2(true);
-    }
+  const showSignUp = () => {
+    setShow1(false);
+    setShow2(true);
+  };
 
-    const showLogin = () => {
-        setShow2(false);
-        setShow1(true);
-    }
+  const showLogin = () => {
+    setShow2(false);
+    setShow1(true);
+  };
 
-    const handleSubmitSignUp = async (e) => {
-      e.preventDefault();
-      console.log(email, username, password, passwordRetype);
-    }
+  const handleSubmitSignUp = async (e) => {
+    e.preventDefault();
 
-    const handleSubmitLogin = async (e) => {
-      e.preventDefault();
-      console.log(username, password);
-    }
+    await signup(email, username, password);
+    //console.log(email, username, password, passwordRetype);
+  };
 
+  const handleSubmitLogin = async (e) => {
+    e.preventDefault();
+    //console.log(username, password);
+  };
 
-    return (
-      <>
-        <Link to="" onClick={handleShow1} className="nav-link active">
-          Login
-        </Link>
-  
-        <Modal show={show1} onHide={handleClose1} centered>
-          <Modal.Header closeButton>
-            <Modal.Title>Login</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Form>
-              <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                <Form.Label>Username</Form.Label>
-                <Form.Control
-                  type="text"
-                  onChange={(e) => setUsername(e.target.value)}
-                  value={username}
-                  autoFocus
-                />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                  type="password"
-                  onChange={(e) => setPassword(e.target.value)}
-                  value={password}
-                  autoFocus
-                />
-              </Form.Group>
-            </Form>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="primary" onClick={handleSubmitLogin}>
-              Login
-            </Button>
-            <Button variant="secondary" onClick={showSignUp}>
-              Sign Up
-            </Button>
-          </Modal.Footer>
-        </Modal>
+  return (
+    <>
+      <Link to="" onClick={handleShow1} className="nav-link active">
+        Login
+      </Link>
 
-
-        <Modal show={show2} onHide={handleClose2} centered>
-          <Modal.Header closeButton>
-            <Modal.Title>Create an Account</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Form>
+      <Modal show={show1} onHide={handleClose1} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>Login</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                <Form.Label>Email</Form.Label>
-                <Form.Control
-                  type="email"
-                  onChange={(e) => setEmail(e.target.value)}
-                  value={email}
-                  autoFocus
-                />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
-                <Form.Label>Username</Form.Label>
-                <Form.Control
-                  type="text"
-                  onChange={(e) => setUsername(e.target.value)}
-                  value={username}
-                  autoFocus
-                />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="exampleForm.ControlInput3">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                  type="password"
-                  onChange={(e) => setPassword(e.target.value)}
-                  value={password}
-                  autoFocus
-                />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="exampleForm.ControlInput4">
-                <Form.Label>Re-type Password</Form.Label>
-                <Form.Control
-                  type="password"
-                  onChange={(e) => setPasswordRetype(e.target.value)}
-                  value={passwordRetype}
-                  autoFocus
-                />
-              </Form.Group>
-            </Form>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="primary" onClick={handleSubmitSignUp}>
-              Create
-            </Button>
-            <Button variant="secondary" onClick={showLogin}>
-              Login to existing account
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      </>
-    );
-  }
+              <Form.Label>Username</Form.Label>
+              <Form.Control
+                type="text"
+                onChange={(e) => setUsername(e.target.value)}
+                value={username}
+                autoFocus
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                autoFocus
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleSubmitLogin}>
+            Login
+          </Button>
+          <Button variant="secondary" onClick={showSignUp}>
+            Sign Up
+          </Button>
+        </Modal.Footer>
+      </Modal>
 
-export default LoginAndRegister
+      <Modal show={show2} onHide={handleClose2} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>Create an Account</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                autoFocus
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
+              <Form.Label>Username</Form.Label>
+              <Form.Control
+                type="text"
+                onChange={(e) => setUsername(e.target.value)}
+                value={username}
+                autoFocus
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput3">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                autoFocus
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput4">
+              <Form.Label>Re-type Password</Form.Label>
+              <Form.Control
+                type="password"
+                onChange={(e) => setPasswordRetype(e.target.value)}
+                value={passwordRetype}
+                autoFocus
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            variant="primary"
+            onClick={handleSubmitSignUp}
+            disabled={isLoading}
+          >
+            Create
+          </Button>
+          <Button variant="secondary" onClick={showLogin}>
+            Login to existing account
+          </Button>
+        </Modal.Footer>
+        {error && 
+          <Alert variant="danger" style={{marginBottom: 0}}>
+            {error}
+          </Alert>}
+      </Modal>
+    </>
+  );
+}
+
+export default LoginAndRegister;
--- client/src/hooks/useSignup.js
+++ client/src/hooks/useSignup.js
@@ -4,6 +4,33 @@ import { useAuthContext } from './useAuthContext'
 export const useSignup = () => {
     const [error, setError] = useState(null)
     const [isLoading, setIsLoading] = useState(null)
+    const { dispatch } = useAuthContext()
 
-    
+    const signup = async (email, username, password, roles) => {
+        setIsLoading(true)
+        setError(null)
+        roles = ["User"]
+        const response = await fetch('http://localhost:3500/users', {
+            method: 'POST', 
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({email, username, password, roles})
+        })
+        const json = await response.json()
+
+        if (!response.ok) {
+            setIsLoading(false)
+            setError(json.error)
+        }
+        if (response.ok) {
+            //save the user to local storage
+            localStorage.setItem('user', JSON.stringify(json))
+
+            // update the auth context
+            dispatch({type: 'LOGIN', payload: json})
+
+            setIsLoading(false)
+        }
+
+    }
+    return { signup, isLoading, error}
 }
